987bb70112b12fd398bc8a1deba3984e7d6fa0cc
Kisakye Paul <116822457+PaulDeveloper73@users.noreply.github.com>
2025-11-24 10:23:45 +0300

    added the question list

---
+++ assets/ls/quiz-questions.js
@@ -0,0 +1,48 @@
+// object array to hold  5 quiz questions and answers
+const quizQuestions = [
+  {
+    id: 1,
+    question: 'What is the capital of France?',
+    options: ['Berlin', 'Madrid', 'Paris', 'Rome'],
+    correct_answer: 'Paris',
+    user_answer: null
+  },
+  {
+    id: 2,
+    question: 'Which planet is known as the Red Planet?',
+    options: ['Earth', 'Mars', 'Jupiter', 'Saturn'],
+    correct_answer: 'Mars',
+    user_answer: null
+  },
+  {
+    id: 3,
+    question: 'What is the largest ocean on Earth?',
+    options: [
+      'Atlantic Ocean',
+      'Indian Ocean',
+      'Arctic Ocean',
+      'Pacific Ocean'
+    ],
+    correct_answer: 'Pacific Ocean',
+    user_answer: null
+  },
+  {
+    id: 4,
+    question: "Who wrote 'Romeo and Juliet'?",
+    options: [
+      'Charles Dickens',
+      'William Shakespeare',
+      'Mark Twain',
+      'Jane Austen'
+    ],
+    correct_answer: 'William Shakespeare',
+    user_answer: null
+  },
+  {
+    id: 5,
+    question: 'What is the chemical symbol for gold?',
+    options: ['Au', 'Ag', 'Fe', 'Pb'],
+    correct_answer: 'Au',
+    user_answer: null
+  }
+]
