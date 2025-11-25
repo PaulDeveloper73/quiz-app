8b7cb0aed7878bc4ee091c29319dc98739955c28
Kisakye Paul <116822457+PaulDeveloper73@users.noreply.github.com>
2025-11-25 12:58:40 +0300

    Activated results to be shown after the quiz

--- assets/ls/main-script.js
+++ assets/ls/main-script.js
@@ -19,13 +19,31 @@ const initApp = () => {
   const handleNextBrn = () => {
     fetchQnItem = quizQuestions[qnId] ?? ''
     displayQuiz(qnId, fetchQnItem, quizContainer)
-    qnId++
     if (qnId >= quizQuestions.length) {
+      const getUserAnswers = loadQuizData()
+      const userAnswers = getUserAnswers.filter(
+        qn => qn.correct_answer === qn.user_answer
+      ).length
+
+      // load quiz data from local storage
       nextBtn.style.display = 'none'
       quizHeader.textContent = 'Quiz Completed!'
-      quizContainer.innerHTML =
-        '<h4>Thank you for participating in the quiz.</h4>'
+      quizContainer.innerHTML = `<h4>Thank you for participating in the quiz.</h4> You scored:${userAnswers} out of ${quizQuestions.length}.
+      <button id="restartBtn" onclick="location.reload()">Restart Quiz</button>
+      `
+      // displayResults(quizQuestions, quizContainer)
+      // return
     }
+
+    // check if there is no user answer  and disable the next button
+    const currentQnItem = quizQuestions.find(qn => qn.id === qnId + 1)
+
+    if (currentQnItem.user_answer === null) {
+      nextBtn.disabled = true
+    } else {
+      nextBtn.disabled = false
+    }
+    qnId++
   }
   // add event listner on click on the list to give bgcolor on correct and wrong answer
   quizContainer.addEventListener('click', e => {
@@ -52,13 +70,42 @@ const initApp = () => {
         }
       })
 
+      // update the quizQuestions array
+      // quizQuestions.splice(0, quizQuestions.length, ...updateUserQn)
+      //  updateQuizQuestion(qnId, userAnswer)
+
       // TODO update the quiz question and add to local storage and also get from local storage when the app load
-      // quizQuestions = updateQnObject(id, userAnswer)
-      // console.log(quizQuestions[id]) //
+      const updateUserQn = quizQuestions.map(qn => {
+        if (qn.id === currentQnItem.id) {
+          return {
+            ...qn,
+            user_answer: userAnswer
+          }
+        }
+        return qn
+      })
+      // update the quizQuestions array
+      quizQuestions.splice(0, quizQuestions.length, ...updateUserQn)
+      // enable the next button
+      nextBtn.disabled = false
+      //  save to local storage
+      storeQuizData()
     }
   })
+  const storeQuizData = () => {
+    localStorage.setItem('quizData', JSON.stringify(quizQuestions))
+  }
 
-  const updateQnObject = (id, userAnswer) => {
+  const loadQuizData = () => {
+    const storedQuizData = localStorage.getItem('quizData')
+    if (storedQuizData) {
+      const parsedQuizData = JSON.parse(storedQuizData)
+      // update the quizQuestions array
+      return quizQuestions.splice(0, quizQuestions.length, ...parsedQuizData)
+    }
+  }
+
+  const updateQuizQuestion = (id, userAnswer) => {
     const updateUserQn = quizQuestions.map(qn => {
       if (qn.id === id) {
         return {
